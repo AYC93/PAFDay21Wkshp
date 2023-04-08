d00585fbe86f7ae879c1d48969805b9015930949
AYC93 <ayc.leo@gmail.com>
2023-04-08 14:17:29 +0800

    add sql

---
+++ sql/orders.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM demo.orders
+LIMIT 0, 1000
+
+-- Date: 2023-04-08 14:16
+*/
+INSERT INTO `` (`id`,`order_date`,`shipping_date`,`items`,`price`,`employee_id`) VALUES (1,'2022-01-03','2022-03-01','Sunny',9.99,1);
+INSERT INTO `` (`id`,`order_date`,`shipping_date`,`items`,`price`,`employee_id`) VALUES (2,'2022-01-04','2022-03-02','Moon',10.99,3);
+INSERT INTO `` (`id`,`order_date`,`shipping_date`,`items`,`price`,`employee_id`) VALUES (3,'2022-01-05','2022-03-03','Dream',11.00,4);
+INSERT INTO `` (`id`,`order_date`,`shipping_date`,`items`,`price`,`employee_id`) VALUES (4,'2022-01-06','2022-03-04','Wonder',5.99,5);
+INSERT INTO `` (`id`,`order_date`,`shipping_date`,`items`,`price`,`employee_id`) VALUES (5,'2022-01-07','2022-03-05','Titanic',3.99,2);
